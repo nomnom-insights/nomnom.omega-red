1d99258f8eca9140720ea6211f9931c36a10d16f
Łukasz Korecki <lukasz@korecki.me>
2020-07-05 10:38:56 -0700

    Update dependencies

--- project.clj
+++ project.clj
@@ -6,11 +6,11 @@
             :year 2018
             :key "mit"}
   :deploy-repositories {"clojars" {:sign-releases false
-                                   :username [:gpg :env/clojars_username]
-                                   :password [:gpg :env/clojars_password]}}
-  :dependencies [[org.clojure/clojure "1.10.0"]
-                 [com.stuartsierra/component "0.4.0"]
+                                   :username  :env/clojars_username
+                                   :password :env/clojars_password}}
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [com.stuartsierra/component "1.0.0"]
                  [com.taoensso/carmine "2.19.1"]]
   :plugins [[lein-cloverage "1.0.13" :exclusions [org.clojure/clojure]]]
   :profiles {:dev
-             {:dependencies  [[org.clojure/tools.logging "0.5.0-alpha.1"]]}})
+             {:dependencies  [[org.clojure/tools.logging "1.1.0"]]}})
