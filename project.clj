2ea8e864479d107ddc69e7018239c83a49dc4c00
Łukasz Korecki <lukasz@korecki.me>
2022-03-01 13:35:44 -0800

    Update dependencies

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject nomnom/omega-red "1.0.2"
+(defproject nomnom/omega-red "1.1.0-SNAPSHOT-0"
   :description "Component firendly Redis client, based on Carmine"
   :url "https://github.com/nomnom-insights/nomnom.omega-red"
   :license {:name "MIT License"
@@ -9,8 +9,8 @@
                                    :username :env/clojars_username
                                    :password  :env/clojars_password}}
   :dependencies [[org.clojure/clojure "1.10.3"]
-                 [com.stuartsierra/component "1.0.0"]
+                 [com.stuartsierra/component "1.1.0"]
                  [com.taoensso/carmine "3.1.0"]]
   :global-vars {*warn-on-reflection* true}
   :profiles {:dev
-             {:dependencies  [[org.clojure/tools.logging "1.1.0"]]}})
+             {:dependencies  [[org.clojure/tools.logging "1.2.4"]]}})
